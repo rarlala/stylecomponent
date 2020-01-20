3b71a6e3dfe5053ca20fea64b9700b5adbb3f80a
JEONGSUJI <wtw9611@gmail.com>
2020-01-20 19:07:47 +0900

    final commit

--- hi/App.js
+++ hi/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import style from "styled-components";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 100px;
+`;
+
+const Title = style.Text`
+  font-weight: 600;
+  font-size: 32px;
+`;
 
 export default function App() {
   return (
