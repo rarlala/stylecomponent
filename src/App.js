1213045f5950fca937efa1d50ed9e5795faf1572
JEONGSUJI <wtw9611@gmail.com>
2020-01-20 18:08:53 +0900

    animations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, css, keyframes } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -13,7 +13,7 @@ class App extends Component{
       <Container>
         <GlobalStyle />
         <Button> Hello </Button>
-        <Button danger> Hello </Button>
+        <Button danger rotationTime={3}> Hello </Button>
         <Anchor as="a" href="http://google.com">Go to google</Anchor>
       </Container>
     );
@@ -36,12 +36,26 @@ const Button = styled.button`
   cursor: pointer;
   &:focus{
     outline:none;
-  }
-  background-color: ${props => props.danger ?  "#e74c3c" : "#2ecc71"}
+  };
+  background-color: ${props => props.danger ?  "#e74c3c" : "#2ecc71"};
+  ${props => {
+    if(props.danger){
+      return css`animation: ${rotation} ${props.rotationTime}s linear infinite`;
+    }
+  }}
 `;
 
 const Anchor = styled(Button)`
   text-decoration: none;
+`;
+
+const rotation = keyframes`
+  from{ 
+    transform: rotate(0deg);
+  }
+  to{ 
+    transform: rotate(300deg);
+  }
 `;
 
 export default App;
